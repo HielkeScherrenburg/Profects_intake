6545b328f8d05b583f8ba04731e0b2a87c75e555
Hielke Scherrenburg <162301131+HielkeScherrenburg@users.noreply.github.com>
2025-12-11 17:13:18 +0100

    Delete standard laravel things we don't need

--- Profects_API/app/Models/User.php
+++ Profects_API/app/Models/User.php
@@ -18,8 +18,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
@@ -30,7 +29,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
